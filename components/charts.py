6f8b1a2a41737c41c2810c24b2627582e0e486cc
Roony-6 <roony7.rrc@gmail.com>
2026-05-05 16:17:49 -0600

    feat: add dynamic boxplot for daily usage hours by education level and gender

--- components/charts.py
+++ components/charts.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 from utils import data_processor as dp
+import seaborn as sns
+import plotly.express as px
 
 def grafico_barras(df:pd.DataFrame):
     """
@@ -12,3 +14,23 @@ def grafico_barras(df:pd.DataFrame):
 def grafico_impacto_proposito(serie_impacto):
     """Renderiza un gráfico de barras para el impacto según el propósito."""
     st.bar_chart(serie_impacto)
+    
+    
+def grafico_demografia_horas(df, desglosar_genero, desglosar_educacion):
+    """
+    Renderiza un boxplot dinámico basado en las selecciones del usuario
+    """
+    if desglosar_educacion and desglosar_genero:
+        fig = px.box(df,x="Education_Level",y="Daily_Usage_Hours",color="Gender",points="all",title="Distribución de Horas por Nivel Educativo y Género")
+    elif desglosar_genero:
+        fig = px.box(df,x="Gender",y="Daily_Usage_Hours",points="all",title="Distribución de Horas por Género")
+    elif desglosar_educacion:
+        fig = px.box(df,x="Education_Level",y="Daily_Usage_Hours",points="all",title="Distribución de Horas por Nivel Educativo")
+    else:
+        fig = px.box(
+            df, 
+            y="Daily_Usage_Hours", 
+            points="all",
+            title="Distribución General de Horas de Uso Diario"
+        )
+    st.plotly_chart(fig)
